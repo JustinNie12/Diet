7bb038e50be57d7c9fa5e93c87bf653b2297c451
JustinNie12 <56981336+JustinNie12@users.noreply.github.com>
2020-10-13 11:34:20 -0500

    Added userInfo
    It might not work

---
+++ UserInfo.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {withGoogleSheets} from 'react-db-google-sheets';
+
+const UserInfo = props => (
+  <div>
+    {props.db.sheet1.map(data => (<span>{data.id}</span>))}
+  </div>
+);
+
+UserInfo.propTypes = {
+  db: PropTypes.shape({
+    sheet1: PropTypes.arrayOf(PropTypes.object)
+  })
+}
+export default withGoogleSheets('sheet1')(UserInfo);
