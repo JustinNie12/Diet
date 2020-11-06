27c5687b2df27a828fd7e146eb3fb6c65ea8d5eb
Dkarpov3 <71338938+Dkarpov3@users.noreply.github.com>
2020-11-06 11:20:25 -0600

    Stores type of workout done

---
+++ dbo.TypeOfWork.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TypeOfWork] (
+    [Id]          INT        NOT NULL,
+    [Date]        DATE       NULL,
+    [WorkoutType] NTEXT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
