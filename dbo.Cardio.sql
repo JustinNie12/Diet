95c5e616e3d2152983025a9842962d0550783b41
Dkarpov3 <71338938+Dkarpov3@users.noreply.github.com>
2020-11-06 11:14:46 -0600

    Add files via upload

---
+++ dbo.Cardio.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Cardio] (
+    [Cardio True/False]       BIT      NOT NULL,
+    [Running]  NTEXT    NULL,
+    [Biking]   NTEXT NULL,
+    [Swimming] NTEXT NULL,
+    [Walking]  NTEXT NULL,
+    PRIMARY KEY CLUSTERED ([Cardio True/False] ASC)
+);
+
