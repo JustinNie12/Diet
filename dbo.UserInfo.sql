03038be76939f02c3b018ea523acae7be4b7a352
Dkarpov3 <71338938+Dkarpov3@users.noreply.github.com>
2020-11-06 11:21:17 -0600

    Data Table for user info

---
+++ dbo.UserInfo.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[UserInfo] (
+    [UserId]      INT        NOT NULL,
+    [UserName]    TEXT       NOT NULL,
+    [Weight]      INT        NOT NULL,
+    [Height]      INT        NOT NULL,
+    [Age]         NCHAR (10) NOT NULL,
+    [DateOfBirth] DATE       NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
