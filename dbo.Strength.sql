4507a728d704a14d808cbd7b2c92d32b0f133798
Dkarpov3 <71338938+Dkarpov3@users.noreply.github.com>
2020-11-06 11:16:24 -0600

    Table for strength workouts

---
+++ dbo.Strength.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Strength] (
+    [Strength Workout]         BIT        NOT NULL,
+    [Benchpress Reps] NCHAR (10) NULL,
+    [Benchpress Rounds]      NCHAR (10) NULL,
+    [Squats Reps] NCHAR (10) NULL,
+    [Squats Rounds]   NCHAR (10) NULL,
+    [Powerclean Reps] NCHAR(10) NULL, 
+    [Powerclean Rounds] NCHAR(10) NULL, 
+    [Deadlift Reps] NCHAR(10) NULL, 
+    [Deadlift Rounds] NCHAR(10) NULL, 
+    PRIMARY KEY CLUSTERED ([Strength Workout] ASC)
+);
+
